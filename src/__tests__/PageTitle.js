5038227a16c6d185df23c0d013f49e57940ecb53
Elena Rakhimova <elenara@kth.se>
2019-12-18 11:33:38 +0100

    Added test for PageTitle

---
+++ src/__tests__/PageTitle.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import renderer from 'react-test-renderer'
+import PageTitle from '../lib/PageTitle'
+
+const TEST_DATA = {
+  mainTitle: 'Redigera introduktion till kursen',
+  pageSubtitle: 'SF1624 Algebra och geometri 7,5 hp'
+}
+
+describe('PageTitle ', () => {
+  it('renders properly', () => {
+    const tree = renderer
+      .create(<PageTitle id='mainHeading' pageTitle={TEST_DATA.mainTitle}>
+        <span>{TEST_DATA.pageSubtitle}</span>
+      </PageTitle>
+      ).toJSON()
+
+    expect(tree).toMatchSnapshot()
+  })
+})
