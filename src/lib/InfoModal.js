e215dd2eed2f7fd81259578e546c8c26fe34214b
Elena Rakhimova <elenara@kth.se>
2019-12-18 17:06:08 +0100

    added missing return statement

--- src/lib/InfoModal.js
+++ src/lib/InfoModal.js
@@ -3,22 +3,23 @@ import { Button, Modal, ModalBody, ModalFooter } from 'reactstrap'
 import ModalHeader from './ModalHeader'
 
 const InfoModal = ({ id, isOpen, header, body, children, closeLabel, onToggle }) => {
-  <Modal isOpen={isOpen} toggle={onToggle} id={id}>
-    <ModalHeader header={header} >
-      <button type='button' className='close' aria-label={'Close'} onClick={onToggle}>
-        <span aria-hidden='true'>×</span>
-      </button>
-    </ModalHeader>
-    <ModalBody>
-      {children}
-      {body && <p dangerouslySetInnerHTML={{ __html: body }}></p>}
-    </ModalBody>
-    <ModalFooter>
-      <Button color='secondary' onClick={onToggle}>
-        {closeLabel}
-      </Button>
-    </ModalFooter>
-  </Modal>
+  return (
+    <Modal isOpen={isOpen} toggle={onToggle} id={id}>
+      <ModalHeader header={header} >
+        <button type='button' className='close' aria-label={'Close'} onClick={onToggle}>
+          <span aria-hidden='true'>×</span>
+        </button>
+      </ModalHeader>
+      <ModalBody>
+        {children}
+        {body && <p dangerouslySetInnerHTML={{ __html: body }}></p>}
+      </ModalBody>
+      <ModalFooter>
+        <Button color='secondary' onClick={onToggle}>
+          {closeLabel}
+        </Button>
+      </ModalFooter>
+    </Modal>
+  )
 }
-
 export default InfoModal
