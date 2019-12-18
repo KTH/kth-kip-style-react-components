8bdf8f790ef3e025cd2f95a6dfaa8e7848db5e11
Elena Rakhimova <elenara@kth.se>
2019-12-18 16:21:17 +0100

    import react for const components

--- src/lib/InfoModal.js
+++ src/lib/InfoModal.js
@@ -1,3 +1,4 @@
+import React from 'react'
 import { Button, Modal, ModalBody, ModalFooter } from 'reactstrap'
 import ModalHeader from './ModalHeader'
 
--- src/lib/ModalHeader.js
+++ src/lib/ModalHeader.js
@@ -1,7 +1,9 @@
- const ModalHeader = ({header, children}) => {
-   <div className='modal-header h-4'>
-     <h4 className='modal-title'>{header}</h4>
-     {children}
-   </div>
- }
- export default ModalHeader
+import React from 'react'
+
+const ModalHeader = ({header, children}) => {
+  <div className='modal-header h-4'>
+    <h4 className='modal-title'>{header}</h4>
+    {children}
+  </div>
+}
+export default ModalHeader
