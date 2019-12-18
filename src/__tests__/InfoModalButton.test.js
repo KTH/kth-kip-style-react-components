d5e411c59c2ef8b6859efef138cbdf7b19974f13
Elena Rakhimova <elenara@kth.se>
2019-12-18 15:37:05 +0100

    InfoModalButton test refactor

---
+++ src/__tests__/InfoModalButton.test.js
@@ -0,0 +1,22 @@
+import React from 'react'
+import renderer from 'react-test-renderer'
+import InfoModalButton from '../lib/InfoModalButton'
+
+const TEST_INFO_FOR_IMAGE = {
+  header: 'Choose image',
+  body: `Choose the image that will be displayed on the page Course information. 
+    You can choose a default image based on the main subject of the course or choose to upload an image on your own choice. 
+    The image will be displayed with the format 300px * 400px. The file format must be .png or .jpg.`,
+  btnCancel: 'Close'
+}
+
+describe('InfoModalButton', () => {
+  it('renders properly', () => {
+    const tree = renderer
+      .create(<InfoModalButton id='infoPic'
+        modalLabels={TEST_INFO_FOR_IMAGE} className='modal-for-info-for-picture' />
+      ).toJSON()
+
+    expect(tree).toMatchSnapshot()
+  })
+})
