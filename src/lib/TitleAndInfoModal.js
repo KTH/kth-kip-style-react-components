8c006edaba62980341aaffe2054f9ab4d5404299
Elena Rakhimova <elenara@kth.se>
2020-01-08 13:24:23 +0100

    Styles: added styles to TitleAndInfoModal component

--- src/lib/TitleAndInfoModal.js
+++ src/lib/TitleAndInfoModal.js
@@ -1,6 +1,20 @@
 import React from 'react'
 import InfoModalButton from './InfoModalButton'
 
+const styles = {
+  span: {
+    display: 'flex',
+    flexDirection: 'row',
+    alignItems: 'flex-start'
+  },
+  btnInfoModal: {
+    // /specific for this project
+    verticalAlign: 'middle'
+  },
+  h3: {
+    marginTop: '0'
+  }
+}
 const TitleAndInfoModal = ({ modalId, titleAndInfo, btnClose }) => {
   const { header, body } = titleAndInfo
   const infoModalLabels = {
@@ -9,10 +23,11 @@ const TitleAndInfoModal = ({ modalId, titleAndInfo, btnClose }) => {
     btnClose: btnClose || 'Close'
   }
   return (
-    <span className='title-and-info'>
-      <h3 data-testid={modalId + '-heading'}>
+    <span className='title-and-info' style={styles.span}>
+      <h3 data-testid={modalId + '-heading'} style={styles.h3}>
         {header}
         <InfoModalButton
+          style={styles.btnInfoModal}
           modalId={modalId + '-infoModal'}
           modalLabels={infoModalLabels}
         />
