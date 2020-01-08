5dae1fbc31d65a95d818de1bf925b7ed0c620722
Elena Rakhimova <elenara@kth.se>
2020-01-08 15:47:16 +0100

    removed stylesheet

--- src/lib/index.js
+++ src/lib/index.js
@@ -3,6 +3,5 @@ import ProgressBar from './ProgressContainer'
 import InfoModalButton from './InfoModalButton'
 import ActionModalButton from './ActionModalButton'
 import TitleAndInfoModal from './TitleAndInfoModal'
-import '../scss/Components.scss'
 
 export { PageTitle, ProgressBar, TitleAndInfoModal, InfoModalButton, ActionModalButton }
