c29b434b86060fbddb7a784827d2b8345de167e0
Elena Rakhimova <elenara@kth.se>
2020-01-08 15:28:02 +0100

    Style: a try to use scss stylesheet

--- src/lib/index.js
+++ src/lib/index.js
@@ -3,5 +3,6 @@ import ProgressBar from './ProgressContainer'
 import InfoModalButton from './InfoModalButton'
 import ActionModalButton from './ActionModalButton'
 import TitleAndInfoModal from './TitleAndInfoModal'
+import '..scss/Components.scss'
 
 export { PageTitle, ProgressBar, TitleAndInfoModal, InfoModalButton, ActionModalButton }
