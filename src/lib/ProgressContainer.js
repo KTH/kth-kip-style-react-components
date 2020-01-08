9ac9beb7a1cd9e32830f4930dac0a3bc69959434
Elena Rakhimova <elenara@kth.se>
2020-01-08 16:04:56 +0100

    Styles: Added inline styles to ProgressContainer

--- src/lib/ProgressContainer.js
+++ src/lib/ProgressContainer.js
@@ -1,7 +1,8 @@
 
 import React from 'react'
 import { Row } from 'reactstrap'
-/*
+
+/* EXAMPLE
  pages: [
     {
       title: '1. Choose course offering',
@@ -24,16 +25,37 @@ import { Row } from 'reactstrap'
     }
   ]
 */
+const styles = {
+  general: {
+    margin: '0 15px 50px 15px'
+  },
+  p: {
+    marginTop: '20px',
+    marginBottom: '0'
+  },
+  progressInactive: {
+    borderBottom: '2px solid #d4d4d4',
+    paddingLeft: '0',
+    marginBottom: '10px'
+  },
+  progressActive: {
+    borderBottom: '2px solid #d4d4d4',
+    paddingLeft: '0',
+    marginBottom: '10px',
+    fontWeight: '900'
+  }
+}
 const ProgressBar = ({active, pages}) => {
   const pagesIndex = active - 1
   return (
-    <Row className='progress-bar-container'>
-      {pages.map((page, index) => <div className={`col-md-4 col-sm-12 progress-bar1 ${pagesIndex === index ? 'progress-active' : ''}`} key={page.title}>
+    <Row className='progress-bar-container' style={styles.general}>
+      {pages.map((page, index) => <div className={`col-md-4 col-sm-12 progress-bar1 ${pagesIndex === index ? 'progress-active' : ''}`}
+        style={pagesIndex === index ? styles.progressActive : styles.progressActive} key={page.title}>
         <span>{page.title}</span>
       </div>
       )}
       <div>
-        <p data-testid='intro-text'>{pages[pagesIndex].intro}</p>
+        <p data-testid='intro-text' style={styles.p}>{pages[pagesIndex].intro}</p>
       </div>
     </Row>
   )
