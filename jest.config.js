b4dc777472d7c20af55398788a5dd9319b559170
Elena Rakhimova <elenara@kth.se>
2020-12-10 11:39:17 +0100

    fix: remove jest config

--- jest.config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  globals: {
-    NODE_ENV: 'test',
-  },
-  clearMocks: true,
-  notifyMode: 'failure-change',
-  transformIgnorePatterns: ['node_modules/(?!(@kth|@babel|@jest)/)'],
-  moduleFileExtensions: ['js', 'jsx', 'json', 'node'],
-}
