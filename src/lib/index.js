ab6fcb52ae844e591df82e6de9d88dd8604d01d8
Elena Rakhimova <elenara@kth.se>
2020-12-10 11:32:29 +0100

    fix: update index js

--- src/lib/index.js
+++ src/lib/index.js
@@ -1,3 +1,4 @@
+import Breadcrumbs from './Breadcrumbs'
 import Collapse from './Collapse'
 import PageTitle from './PageTitle'
 import ProgressBar from './ProgressContainer'
@@ -5,4 +6,14 @@ import InfoModalButton from './InfoModalButton'
 import ActionModalButton from './ActionModalButton'
 import TitleAndInfoModal from './TitleAndInfoModal'
 import SectionTitleAndInfoModal from './SectionTitleAndInfoModal'
-export { Collapse, PageTitle, ProgressBar, TitleAndInfoModal, SectionTitleAndInfoModal, InfoModalButton, ActionModalButton }
+
+export {
+  Breadcrumbs,
+  Collapse,
+  PageTitle,
+  ProgressBar,
+  TitleAndInfoModal,
+  SectionTitleAndInfoModal,
+  InfoModalButton,
+  ActionModalButton,
+}
